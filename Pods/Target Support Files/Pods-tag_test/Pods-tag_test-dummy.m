1cd48423b9e41265b3be9346a6b5fc2376229bfa
Jim Smith <jimsmithm3@gmail.com>
2017-08-13 12:52:13 -0500

    init

---
+++ Pods/Target Support Files/Pods-tag_test/Pods-tag_test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tag_test : NSObject
+@end
+@implementation PodsDummy_Pods_tag_test
+@end
---
+++ Pods/Target Support Files/Pods-tag_test/Pods-tag_test-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_tag_testVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_tag_testVersionString[];
+
